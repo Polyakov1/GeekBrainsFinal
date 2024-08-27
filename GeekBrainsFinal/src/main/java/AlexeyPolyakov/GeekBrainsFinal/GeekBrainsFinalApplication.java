a40c84e929c7751332de44cbf1743f217dfa59da
Alexey Polyakov <slach93@mail.ru>
2024-08-27 16:34:51 +0500

    first commit

---
+++ GeekBrainsFinal/src/main/java/AlexeyPolyakov/GeekBrainsFinal/GeekBrainsFinalApplication.java
@@ -0,0 +1,13 @@
+package AlexeyPolyakov.GeekBrainsFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekBrainsFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekBrainsFinalApplication.class, args);
+	}
+
+}
---
+++ GeekBrainsFinal/src/test/java/AlexeyPolyakov/GeekBrainsFinal/GeekBrainsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package AlexeyPolyakov.GeekBrainsFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekBrainsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
